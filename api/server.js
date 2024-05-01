fe5be26925cd95e6821145c3dd203c1e0e5bc7ef
unknown <rifqirm15122001@gmail.com>
2024-05-01 23:12:54 +0700

    test speed api

--- api/server.js
+++ api/server.js
@@ -10,7 +10,7 @@ const path = require('path')
 const filePath = path.join('db.json')
 const data = fs.readFileSync(filePath, "utf-8");
 const db = JSON.parse(data);
-// const router = jsonServer.router('db.json')
+const router = jsonServer.router('db.json')
 
 // Comment out to allow write operations
 
